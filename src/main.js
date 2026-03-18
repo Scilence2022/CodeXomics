3a63bfe2d47d82678eff56ae6017f2f86d30740a
Lifu Song <switchcodes@yeah.net>
2026-03-18 12:35:59 +0800

    go back a comment

--- src/main.js
+++ src/main.js
@@ -1462,7 +1462,6 @@ function createWindow() {
       cache: false,
       partition: `persist:codexomics-${windowId}`,
       serviceWorkers: false,
-      preload: path.join(__dirname, 'preload.js'),
     },
     icon: path.join(__dirname, '../assets/icon.png'),
     show: false,
